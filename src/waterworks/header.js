261bf74428c886043e93db8d20a980995f943029
Derin Owoade <owoadederin6@gmail.com>
2022-07-08 09:43:34 +0100

    new changes

--- src/waterworks/header.js
+++ src/waterworks/header.js
@@ -1,28 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 
+export default function Header() {
+  const location = useLocation().pathname;
 
-
-
-
-export default function Header(){
-    const location = useLocation().pathname
-    
-    return <div className="flex flex-col p-8 font-Anek w-full ">
-      
-        <div className="flex items-center justify-center">
+  return (
+    <div className="flex flex-col p-8 font-Anek w-full ">
+      <div className="flex items-center justify-center">
         <div className="absolute left-10 hidden sm:block">
-            <div className="flex items-center">
-            <span className="text-green-300 mr-1 font-bold w-fit text-6xl">GO! </span>water by Yinka
-            </div>
+          <div className="flex items-center">
+            <span className="text-green-300 mr-1 font-bold w-fit text-6xl">
+              GO!{" "}
+            </span>
+            water by Yinka
+          </div>
         </div>
-       <Link to="/"> <button className={`px-2 pb-0 text-gray-500 ${location === "/" ? "text-green-400": ""}`}>Home</button></Link>
-        <Link to="/products"><button className={`px-2 pb-0 text-gray-500 ${location === "/products" ? "text-green-400": ""}`}>Products</button></Link>
-        <Link to="/contact"> <button className={`px-2 pb-0 text-gray-500 ${location === "/contact" ? "text-green-400": ""} `}>Contact</button></Link>
+        <Link to="/">
+          {" "}
+          <button
+            className={`px-2 pb-0 text-gray-500 ${
+              location === "/" ? "text-green-400" : ""
+            }`}
+          >
+            Home
+          </button>
+        </Link>
+        <Link to="/products" className="block sm:hidden">
+          <button
+            className={`px-2 pb-0 text-gray-500 ${
+              location === "/products" ? "text-green-400" : ""
+            }`}
+          >
+            Products
+          </button>
+        </Link>
+        <Link to="/contact">
+          {" "}
+          <button
+            className={`px-2 pb-0 text-gray-500 ${
+              location === "/contact" ? "text-green-400" : ""
+            } `}
+          >
+            Contact
+          </button>
+        </Link>
+      </div>
+      <div className="flex items-center block sm:hidden">
+        <span className="text-green-300 mr-1 font-bold w-fit text-6xl">
+          GO!{" "}
+        </span>
+        water by Yinka
+      </div>
     </div>
-    <div className="flex items-center block sm:hidden">
-        <span className="text-green-300 mr-1 font-bold w-fit text-6xl">GO! </span>water by Yinka
-    </div>
-   
-    </div>
+  );
 }
--- src/waterworks/products.js
+++ src/waterworks/products.js
@@ -1,45 +1,44 @@
 import React from "react";
-import SachetWater from "./Media/SachetWater.jpg"
-import LimeWater from "./Media/LimeWater.jpg"
-import WaterDispenser1 from "./Media/WaterDispenser1.jpg"
-import WaterBottle from "./Media/WaterBottle3.jpg"
+import SachetWater from "./Media/SachetWater.jpg";
+import LimeWater from "./Media/LimeWater.jpg";
+import WaterDispenser1 from "./Media/WaterDispenser1.jpg";
+import WaterBottle from "./Media/WaterBottle3.jpg";
 
-
-export default function Products(){
-    return <div className="w-full font-Anek pb-20">
-        <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col md:flex-row md:mr-32 md:mb-10">
-                <img src={SachetWater} className="h-48 md:h-[250px]">
-                </img>
-                <div className="text-3xl text-green-400 p-4 rounded-lg">
-                    SachetWater
-                    <div className="text-sm md:w-96 w-72 text-black">
-                        You can't go wrong with Sachet water, convinient and easy on the wallet. Our Sachet water, like all of our products, undergoes a rigorous purification proccess. It can be found in your nearest convinient store
-                    </div>
-                </div>
-
+export default function Products() {
+  return (
+    <div className="w-full font-Anek pb-20">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col md:flex-row md:mr-32 md:mb-10">
+          <img src={LimeWater} className="h-[300px]"></img>
+          <div className="text-3xl text-green-400 p-6 rounded-lg">
+            LimeWater
+            <div className="text-sm md:w-96 w-72 text-black">
+              Refreshing and full of Citrus goodness.
             </div>
-           
-            <div className="md:mr-32 flex flex-col md:flex-row items-center justify-center md:mb-10">
-                <img src={WaterBottle} className="w-48 md:w-[350px]">
-                </img>
-                <div className="text-3xl text-green-400 p-4">
-                    Bottled Water
-                    <div className="text-sm w-72 md:w-96 text-black">
-                        Our flagship, Distilled, Purified and packed infused with minerals to keep you moving and healthy.
-                    </div>
-                </div>
-            </div>
-            <div className="md:mr-32 flex flex-col md:flex-row items-center justify-center">
-                <img src={WaterDispenser1} className="w-96 p-6">
-                </img>
-                <div className="text-3xl text-green-400 p-4">
-                    Dispeneser Water
-                    <div className="text-sm w-72 md:w-96 text-black">
-                        For the family and Office, contact us to make an order for Dispeneser water.
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+
+        <div className="md:mr-32 flex flex-col md:flex-row items-center justify-center md:mb-10">
+          <img src={WaterBottle} className="w-48 md:w-[350px]"></img>
+          <div className="text-3xl text-green-400 p-4">
+            Bottled Water
+            <div className="text-sm w-72 md:w-96 text-black">
+              Our flagship, Distilled, Purified and packed infused with minerals
+              to keep you moving and healthy.
+            </div>
+          </div>
+        </div>
+        {/* <div className="md:mr-32 flex flex-col md:flex-row items-center justify-center">
+          <img src={WaterDispenser1} className="w-96 p-6"></img>
+          <div className="text-3xl text-green-400 p-4">
+            Dispeneser Water
+            <div className="text-sm w-72 md:w-96 text-black">
+              For the family and Office, contact us to make an order for
+              Dispeneser water.
+            </div>
+          </div>
+        </div> */}
+      </div>
     </div>
+  );
 }
